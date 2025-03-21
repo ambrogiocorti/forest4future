4ff29ba30965ebc7071f6b490d10ec1c3736a4ce
Eli <bernerielia@gmail.com>
2025-03-21 12:49:08 +0100

    updated the css and html

--- scripts/carosello.js
+++ scripts/carosello.js
@@ -3,3 +3,29 @@ let next = document.getElementById("next");
 
 let carosello = 0;
 
+let immagini = [
+    "./images/carosello/image.jpeg",
+    "./images/carosello/image-2.jpeg",
+];
+
+document.getElementById("next").addEventListener("click", avanti);
+document.getElementById("prev").addEventListener("click", indietro);
+
+function immagine() {
+    let divImmagine = document.getElementById("immagini");
+    divImmagine.style.backgroundImage = `url('${immagini[carosello]}')`;
+    divImmagine.style.backgroundSize = "cover";
+    divImmagine.style.backgroundPosition = "center";
+}
+
+function avanti() {
+    carosello = (carosello + 1) % immagini.length;
+    immagine();
+}
+
+function indietro() {
+    carosello = (carosello - 1 + immagini.length) % immagini.length;
+    immagine();
+}
+
+immagine();
